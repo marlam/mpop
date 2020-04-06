3b6074698ddb6308399806c21592d0f50dc3a879
Martin Lambers <marlam@marlam.de>
2020-04-06 21:38:24 +0200

    Update --version message.

--- src/mpop.c
+++ src/mpop.c
@@ -2256,7 +2256,7 @@ int main(int argc, char *argv[])
             free(conffile);
         }
         printf("\n");
-        printf(_("Copyright (C) 2019 Martin Lambers and others.\n"
+        printf(_("Copyright (C) 2020 Martin Lambers and others.\n"
                     "This is free software.  You may redistribute copies of "
                         "it under the terms of\n"
                     "the GNU General Public License "
